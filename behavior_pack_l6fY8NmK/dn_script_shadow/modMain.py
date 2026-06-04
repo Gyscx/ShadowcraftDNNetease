ade515170f7f19619927443812fc1d9a36100837
Gyscx <1250378829@qq.com>
2026-06-04 22:17:08 +0800

    Update modMain.py

--- behavior_pack_l6fY8NmK/dn_script_shadow/modMain.py
+++ behavior_pack_l6fY8NmK/dn_script_shadow/modMain.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+from mod.common.mod import Mod
 from architect.compact import createServer, createClient
+from . import conf
 
-def modInit():
-    # 读取 conf.py 中的 MOD_ENGINE_NAME 和 MOD_SYSTEM_NAME 完成初始化
-    createServer()
-    createClient()
+@Mod.Binding(name = conf('MOD_NAME'), version = conf('MOD_VERSION'))
+class ModBase(object):
+    @Mod.InitServer()
+    def initServer(self):
+        createServer()
+
+    @Mod.InitClient()
+    def initClient(self):
+        createClient()
